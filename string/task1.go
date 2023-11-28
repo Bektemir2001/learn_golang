1c00a697b337af77bb08f61c80c2111f66bc5c29
Bektemir2001 <bektemirkumarbayuulu@gmail.com>
2023-11-28 14:15:51 +0600

    make task 1

---
+++ string/task1.go
@@ -0,0 +1,28 @@
+/*
+	На вход подается строка. Нужно определить, является ли она правильной или нет.
+	Правильная строка начинается с заглавной буквы и заканчивается точкой.
+	Если строка правильная - вывести Right иначе - вывести Wrong
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	strings.TrimSuffix(text, "\n")
+	byteValue := []rune(text)
+	if unicode.IsUpper(byteValue[0]) && strings.HasSuffix(text, ".") {
+		fmt.Println("Right")
+	} else {
+		fmt.Println("Wrong")
+	}
+}
